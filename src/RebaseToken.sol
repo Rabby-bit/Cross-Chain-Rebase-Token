176e8346e4af745a73ced8b352afd15279e7ab72
Rabby-bit <rabiaubaidu@gmail.com>
2026-04-09 17:30:44 +0100

    completed vault

---
+++ src/RebaseToken.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// license
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RebaseToken is ERC20, AccessControl, Ownable {
+    // State variables
+    uint256 private sInterestRate = 5e10;
+    uint256 private constant PRECISION_FACTOR = 1e18;
+    mapping(address user => uint256 amountMinted) private sUserToAmountMinted;
+    mapping(address user => uint256 interestRate) private sUserToInterestRate;
+    mapping(address user => uint256 timeStamp) private sUserToLastUpdatedTimeStamp;
+    bytes32 public constant BURN_MINTER_ROLE = keccak256("BURN_MINTER_ROLE");
+
+    // Events
+    error RebaseToken__InterestRateShouldOnlyDecrease();
+
+    event InterestRateSet(uint256 indexed newInterestRate);
+    // Modifiers
+
+    constructor() ERC20("RebaseToken", "RBT") Ownable(msg.sender) {}
+
+    ///External//
+    function mint(address _user, uint256 _amount) external onlyRole(BURN_MINTER_ROLE) {
+        _mintAccuredInterest(_user);
+        sUserToInterestRate[_user] += sInterestRate;
+        sUserToAmountMinted[_user] += _amount;
+        _mint(_user, _amount);
+    }
+
+    function burn(address _user, uint256 _amount) external onlyRole(BURN_MINTER_ROLE) {
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_user);
+        }
+        _mintAccuredInterest(_user);
+        sUserToAmountMinted[_user] -= _amount;
+        _burn(_user, _amount);
+    }
+
+    function transfer(address _reciepent, uint256 _amount) public override returns (bool) {
+        _mintAccuredInterest(_reciepent);
+        _mintAccuredInterest(msg.sender);
+
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(msg.sender);
+        }
+        return super.transfer(_reciepent, _amount);
+    }
+
+    function transferFrom(address _sender, address _reciepent, uint256 _amount) public override returns (bool) {
+        _mintAccuredInterest(_reciepent);
+        _mintAccuredInterest(_sender);
+
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_sender);
+        }
+
+        return super.transferFrom(_sender, _reciepent, _amount);
+    }
+
+    ///Public//
+    function setInterestRete(uint256 _newInterestRate) public onlyOwner {
+        if (_newInterestRate > sInterestRate) {
+            revert RebaseToken__InterestRateShouldOnlyDecrease();
+        }
+        _newInterestRate = sInterestRate;
+        emit InterestRateSet(_newInterestRate);
+    }
+
+    function grantRole(bytes32 role, address account) public override onlyOwner {
+        _grantRole(BURN_MINTER_ROLE, account);
+    }
+    //Internal//
+
+    //Private//
+
+    //internal & private view/pure//
+
+    function _mintAccuredInterest(address _user) internal view returns (uint256 increaseInBalance) {
+        /// TODO: Implement full logic to calculate and mint actual interest tokens.
+        // The amount of interest to mint would be:
+        // current_dynamic_balance - current_stored_principal_balance
+        // Then, _mint(_user, interest_amount_to_mint);
+        uint256 previousPrincipleBalance = super.balanceOf(_user);
+
+        uint256 dynamicBalance = balanceOf(_user);
+
+        increaseInBalance = dynamicBalance - previousPrincipleBalance;
+    }
+
+    function _calculateAccumulateInterest(address _user) internal view returns (uint256 linearInterest) {
+        uint256 timeElapsed = block.timestamp - sUserToLastUpdatedTimeStamp[_user];
+        linearInterest = PRECISION_FACTOR + (sUserToInterestRate[_user] * timeElapsed);
+    }
+
+    //external & public view/pure //
+    function balanceOf(address _user) public view override returns (uint256) {
+        uint256 principleBalance = super.balanceOf(_user);
+
+        uint256 growthFactor = _calculateAccumulateInterest(_user);
+        return (principleBalance * growthFactor) / PRECISION_FACTOR;
+    }
+
+    function getUserInterestRate(address _user) external view returns (uint256) {
+        return sUserToInterestRate[_user];
+    }
+}
---
+++ src/Vault.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// license
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+pragma solidity ^0.8.24;
+
+import {RebaseToken} from "src/RebaseToken.sol";
+import {iRebaseToken} from "src/iRebaseToken.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Vault is ReentrancyGuard, AccessControl {
+    // State variables
+    iRebaseToken public immutable i_rebaseToken;
+
+    mapping(address user => uint256 amountDeposited) private sUserToAmountDeposited;
+
+    // Events
+    error Vault__CantDepositZero();
+    error Vault__CantRedeemZero();
+    error Vault__WithDrawFailed();
+    error Vault__DepositTransferFailed();
+    error Vault__CantMoreThanDeposited();
+
+    event Deposited(address indexed user, uint256 indexed amount);
+    event Redeemed(address indexed user, uint256 indexed amount);
+
+    // Modifiers
+
+    // constructor
+    constructor(iRebaseToken rebaseTokenAddress) {
+        i_rebaseToken = rebaseTokenAddress;
+    }
+    // receive function
+    receive() external payable {}
+
+    function deposit() external payable {
+        if (msg.value == 0) {
+            revert Vault__CantDepositZero();
+        }
+
+        sUserToAmountDeposited[msg.sender] += msg.value;
+        i_rebaseToken.mint(msg.sender, msg.value);
+
+        emit Deposited(msg.sender, msg.value);
+    }
+
+    function redeem(uint256 _amount) external payable nonReentrant {
+        if (_amount == 0) {
+            revert Vault__CantRedeemZero();
+        }
+        if (sUserToAmountDeposited[msg.sender] < _amount) {
+            revert Vault__CantMoreThanDeposited();
+        }
+
+        sUserToAmountDeposited[msg.sender] -= _amount;
+        i_rebaseToken.burn(msg.sender, _amount);
+
+        (bool success,) = msg.sender.call{value: _amount}("");
+        if (!success) {
+            revert Vault__WithDrawFailed();
+        }
+        emit Redeemed(msg.sender, _amount);
+    }
+}
